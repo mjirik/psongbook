a404c055e5b7b4320b9e6b93909e76584d00866c
Miroslav Jiřík <mjirik@kky.zcu.cz>
2016-05-17 10:40:34 +0200

    new modules added

---
+++ sobo/transpose.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR%  <>
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+import argparse
+
+
+def process_file(filename):
+    text = load_file(filename)
+
+def load_file(txt, ofilename):
+    text = None
+    with open(ofilename, 'r') as f:
+        text = f.read(txt)
+
+    return text
+
+def get_parser(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '-i', '--inputfile',
+        default=None,
+        required=True,
+        help='input file'
+    )
+    parser.add_argument(
+        '-d', '--debug', action='store_true',
+        help='Debug mode')
+    return parser
+
+def main():
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    logger.addHandler(ch)
+
+    # create file handler which logs even debug messages
+    # fh = logging.FileHandler('log.txt')
+    # fh.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter(
+    #     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+    # logger.debug('start')
+
+    # input parser
+    parser = get_parser()
+    args = parser.parse_args()
+
+    process_file(args.inputfile)
+
+
+def main_args(args):
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+
+if __name__ == "__main__":
+    main()
